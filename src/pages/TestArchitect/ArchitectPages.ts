6a40969185f3493393d0d5c12a929c7e55cc548f
thuan.do <thuan.do@agest.vn>
2025-07-21 16:45:00 +0700

    Fix issue login common

--- src/pages/TestArchitect/ArchitectPages.ts
+++ src/pages/TestArchitect/ArchitectPages.ts
@@ -1,5 +1,4 @@
 import { Page } from '@playwright/test';
-
 import { testArchitectUrl } from '../../utils/envConfig';
 import { ArchitectLocators } from '../../locators/TestArchitect/TestArchitectPage.locator';
 
@@ -15,7 +14,7 @@ export class TestArchitectPage {
   }
 
   async closePopupIfVisible(): Promise<void> {
-    const popupCloseBtns = this.page.locator('.pum-close');
+    const popupCloseBtns = this.page.locator(ArchitectLocators.popupCloseButton);
     const count = await popupCloseBtns.count();
     for (let i = 0; i < count; i++) {
       const button = popupCloseBtns.nth(i);
@@ -33,7 +32,37 @@ export class TestArchitectPage {
     await this.page.locator(ArchitectLocators.aboutUsLink).click();
   }
 
+  async clickBlogTab(): Promise<void> {
+    await this.page.locator(ArchitectLocators.blogTabLink).click();
+  }
+
+  async clickContactUsTab(): Promise<void> {
+    await this.page.locator(ArchitectLocators.contactUsTabLink).click();
+  }
+
+  async clickOffersTab(): Promise<void> {
+    await this.page.locator(ArchitectLocators.offersTabLink).click();
+  }
+
+  async clickShopTab(): Promise<void> {
+    await this.page.locator(ArchitectLocators.shopTabLink).click();
+  }
+
+  async clickAboutTab(): Promise<void> {
+    await this.page.locator(ArchitectLocators.aboutTabLink).click();
+  }
+
+  async clickHomeTab(): Promise<void> {
+    await this.page.locator(ArchitectLocators.homeTabLink).click();
+  }
+
   async getTitle(): Promise<string> {
     return await this.page.title();
+  }
+  
+  async gotoAndClickLogin(): Promise<void> {
+    await this.goto();
+    await this.closePopupIfVisible();
+    await this.clickLoginTA();
   }
 }
